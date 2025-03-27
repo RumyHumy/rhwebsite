55b2e8597aa658417e341dfeacecff56e5ee3548
Ramil Khsanshin <khasanshin.ramil@list.ru>
2025-03-27 13:43:39 +0300

    Fullscreen on touch, testing was successeful

--- src/wasmtest/main.c
+++ src/wasmtest/main.c
@@ -1,18 +1,22 @@
 #include "raylib.h"
 #include <math.h>
 #include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
 
 float t = 0.0;
 
 void main_loop(void) {
 	t += GetFrameTime();
+	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+		emscripten_request_fullscreen("#canvas", 1);
+	}
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText(
-		"bootycheeks",
-		sin(t)*100+100,
-		sin(t)*100+100, 100, DARKGRAY);
-    DrawFPS(10, 10);
+		ClearBackground(BLACK);
+		DrawText(
+			"bootycheeks",
+			sin(t)*100+100,
+			sin(t)*100+100, 100, DARKGRAY);
+		DrawFPS(10, 10);
     EndDrawing();
 }
 
