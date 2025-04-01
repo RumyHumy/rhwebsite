19f524d5ae1f9cfab0de6d287acf45bed44048d8
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-01 09:36:03 +0300

    WIP: In process of adding tests

--- src/dd/worker.js
+++ src/dd/worker.js
@@ -1,19 +1,31 @@
 //importScripts("/lib/pyodide/pyodide.js");
+
+// L O A D   P Y O D I D E
+
 importScripts("https://cdn.jsdelivr.net/pyodide/v0.27.4/full/pyodide.js");
+
+var output = "";
+
+function print(msg) {
+	postMessage({ type: 'print', data: msg });
+	output += msg+"\n";
+}
 
 async function loadPyodideAndPackages() {
 	self.pyodide = await loadPyodide({
-		stdout: (text) => { postMessage({ type: 'print', data: text }); },
-		stderr: (text) => { postMessage({ type: 'print', data: text }); },
+		stdout: (text) => { print(text) },
+		stderr: (text) => { print(text) },
 	});
 	postMessage({ type: 'loaded' });
 }
+
 loadPyodideAndPackages()
 
-self.onmessage = async (event) => {
+// R U N   &   T E S T
+
+async function runCode(code) {
 	if (!self.pyodide)
 		return;
-	var { code } = event.data;
 	try {
 		await self.pyodide.runPythonAsync("import js\njs.pyodide.globals.clear()\n");
 		const result = await self.pyodide.runPythonAsync(code);
@@ -22,5 +34,35 @@ self.onmessage = async (event) => {
 		}
 	} catch (error) {
 		postMessage({ type: 'error', data: error.message });
+	}
+}
+
+async function runTests(code, testData) {
+	if (!self.pyodide || !testData)
+		return;
+	try {
+		for (var i = 0; i < testData.length; i++) {
+			await self.pyodide.runPythonAsync("import js\njs.pyodide.globals.clear()\n");
+			const result = await self.pyodide.runPythonAsync(code);
+			if (result !== undefined) {
+				postMessage({ type: 'tests-result', data: result });
+			}
+		}
+	} catch (error) {
+		postMessage({ type: 'error', data: error.message });
+	}
+}
+
+// M E S S A G E S
+
+self.onmessage = async (event) => {
+	const { type, code, testData } = event.data;
+	switch (type) {
+	case 'run':
+		await runCode(code);
+		break;
+	case 'tests':
+		await runTests(code, testData);
+		break;
 	}
 };
