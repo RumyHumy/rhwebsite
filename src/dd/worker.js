2fc708ba869695747939db6181078d020e119fac
Ramil Khsanshin <khasanshin.ramil@list.ru>
2025-04-03 09:54:11 +0300

    WIP: Fixed bug with running 2 programs at once + globals are cleaning in js now

--- src/dd/worker.js
+++ src/dd/worker.js
@@ -24,33 +24,20 @@ loadPyodideAndPackages()
 // R U N   &   T E S T
 
 async function runCode(code) {
+	if (self.pyodide)
+		self.pyodide.globals.clear()
 	if (!self.pyodide)
 		return;
 	try {
-		await self.pyodide.runPythonAsync("import js\njs.pyodide.globals.clear()\n");
 		const result = await self.pyodide.runPythonAsync(code);
-		if (result !== undefined) {
-			postMessage({ type: 'result', data: result });
-		}
+		postMessage({ type: 'result', data: result });
 	} catch (error) {
 		postMessage({ type: 'error', data: error.message });
 	}
 }
 
 async function runTests(code, testData) {
-	if (!self.pyodide || !testData)
-		return;
-	try {
-		for (var i = 0; i < testData.length; i++) {
-			await self.pyodide.runPythonAsync("import js\njs.pyodide.globals.clear()\n");
-			const result = await self.pyodide.runPythonAsync(code);
-			if (result !== undefined) {
-				postMessage({ type: 'tests-result', data: result });
-			}
-		}
-	} catch (error) {
-		postMessage({ type: 'error', data: error.message });
-	}
+	
 }
 
 // M E S S A G E S
