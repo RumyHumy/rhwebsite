ef408b464e0372da8953bfe9771953056d4c8d82
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-07 11:21:54 +0300

    Adding resources script

--- res.sh
+++ res.sh
@@ -1,3 +1,5 @@
-#!#/bin/bash
+#!/bin/bash
 # Additive synchronization for src/res with server
-# 
+# ./res.sh USER IP PORT
+rsync -av -e "ssh -p $3" --ignore-existing ~/rhwebsite/src/res/* $1@$2:~/rhres
+ssh -p $3 $1@$2 "rsync avz ~/rhres ~/rhres.bak"
