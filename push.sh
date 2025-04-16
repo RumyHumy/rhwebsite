09a85850195982aa732c504200eb8e697f848c3a
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-17 00:09:02 +0300

    Refining push.sh script

--- push.sh
+++ push.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 if [ -z $1 ] || [ -z $ssh_user ] || [ -z $ssh_ip ] || [ -z $ssh_port ]; then
 	echo "ssh_user= ssh_ip= ssh_port= ./push.sh -local/-prod"
-	echo "ssh_user= ssh_ip= ssh_port= - ssh server credentials"
-	echo "Access with key!"
 	exit
 fi
 
-rsync -av --delete -e "ssh -p $ssh_port" ~/rhwebsite $ssh_user@$ssh_ip:~ --exclude ./src/res --exclude .git
+rsync -av --delete \
+	-e "ssh -p $ssh_port" \
+	~/rhwebsite $ssh_user@$ssh_ip:~ \
+	--exclude src/res
 
-ssh -p $ssh_port $ssh_user@$ssh_ip "mkdir ~/rhres; ln -s ~/rhres ~/rhwebsite/src/res; cd ~/rhwebsite; ./run.sh $1"
+ssh -p $ssh_port $ssh_user@$ssh_ip "
+mkdir ~/rhres
+ln -s ~/rhres ~/rhwebsite/src/res
+cd ~/rhwebsite
+./run.sh $1
+"
